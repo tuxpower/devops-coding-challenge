9b9bad5952c0b624a972fe305d29731eaa899391
Jose Gaspar <jad.gaspar@gmail.com>
2016-09-21 16:27:46 +0100

    First commit

---
+++ AwsConsoleApp.java
@@ -0,0 +1,193 @@
+/*
+ * Copyright 2010-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
+/**
+ * Welcome to your new AWS Java SDK based project!
+ *
+ * This class is meant as a starting point for your console-based application that
+ * makes one or more calls to the AWS services supported by the Java SDK, such as EC2,
+ * SimpleDB, and S3.
+ *
+ * In order to use the services in this sample, you need:
+ *
+ *  - A valid Amazon Web Services account. You can register for AWS at:
+ *       https://aws-portal.amazon.com/gp/aws/developer/registration/index.html
+ *
+ *  - Your account's Access Key ID and Secret Access Key:
+ *       http://aws.amazon.com/security-credentials
+ *
+ *  - A subscription to Amazon EC2. You can sign up for EC2 at:
+ *       http://aws.amazon.com/ec2/
+ *
+ *  - A subscription to Amazon SimpleDB. You can sign up for Simple DB at:
+ *       http://aws.amazon.com/simpledb/
+ *
+ *  - A subscription to Amazon S3. You can sign up for S3 at:
+ *       http://aws.amazon.com/s3/
+ */
+public class AwsConsoleApp {
+
+    /*
+     * Before running the code:
+     *      Fill in your AWS access credentials in the provided credentials
+     *      file template, and be sure to move the file to the default location
+     *      (/Users/jgaspar/.aws/credentials) where the sample code will load the
+     *      credentials from.
+     *      https://console.aws.amazon.com/iam/home?#security_credential
+     *
+     * WARNING:
+     *      To avoid accidental leakage of your credentials, DO NOT keep
+     *      the credentials file in your source directory.
+     */
+
+    static AmazonS3       s3;
+
+    /**
+     * The only information needed to create a client are security credentials
+     * consisting of the AWS Access Key ID and Secret Access Key. All other
+     * configuration, such as the service endpoints, are performed
+     * automatically. Client parameters, such as proxies, can be specified in an
+     * optional ClientConfiguration object when constructing a client.
+     *
+     * @see com.amazonaws.auth.BasicAWSCredentials
+     * @see com.amazonaws.auth.PropertiesCredentials
+     * @see com.amazonaws.ClientConfiguration
+     */
+    private static void init() throws Exception {
+
+        /*
+         * The ProfileCredentialsProvider will return your [PROFILE-NAME]
+         * credential profile by reading from the credentials file located at
+         * (/Users/jgaspar/.aws/credentials).
+         */
+        AWSCredentials credentials = null;
+        try {
+            credentials = new ProfileCredentialsProvider("PROFILE-NAME").getCredentials();
+        } catch (Exception e) {
+            throw new AmazonClientException(
+                    "Cannot load the credentials from the credential profiles file. " +
+                    "Please make sure that your credentials file is at the correct " +
+                    "location (/Users/jgaspar/.aws/credentials), and is in valid format.",
+                    e);
+        }
+        s3  = new AmazonS3Client(credentials);
+    }
+
+
+    public static void main(String[] args) throws Exception {
+
+        init();
+
+        /*
+         * Amazon S3
+         *
+         * The AWS S3 client allows you to manage buckets and programmatically
+         * put and get objects to those buckets.
+         *
+         * In this sample, we use an S3 client to iterate over all the buckets
+         * owned by the current user, and all the object metadata in each
+         * bucket, to obtain a total object and space usage count. This is done
+         * without ever actually downloading a single object -- the requests
+         * work with object metadata only.
+         */
+        try {
+            List<Bucket> buckets = s3.listBuckets();
+
+            long totalSize  = 0;
+            int  totalItems = 0;
+            
+        	System.out.println("Here's a detailed list of your buckets:");
+        	System.out.println("Name: \t Creation date: \t #Items: \t Size: \t Last Modified:");
+        	
+            for (Bucket bucket : buckets) {
+                /*
+                 * In order to save bandwidth, an S3 object listing does not
+                 * contain every object in the bucket; after a certain point the
+                 * S3ObjectListing is truncated, and further pages must be
+                 * obtained with the AmazonS3Client.listNextBatchOfObjects()
+                 * method.
+                 */
+            	
+            	long bucketSize = 0;
+            	int bucketItems = 0;
+            	DateFormat formatter = new SimpleDateFormat("MM/dd/yy");
+            	Date lastModified = formatter.parse("01/01/70"); 
+                
+            	ObjectListing objects = s3.listObjects(bucket.getName());
+                do {
+                    for (S3ObjectSummary objectSummary : objects.getObjectSummaries()) {
+                        totalSize += objectSummary.getSize();
+                        bucketSize += objectSummary.getSize();
+                        totalItems++;
+                        bucketItems++;
+                        if (objectSummary.getLastModified().after(lastModified)) {
+                    		lastModified = objectSummary.getLastModified();
+                    	}
+               
+                    }
+                    objects = s3.listNextBatchOfObjects(objects);
+                } while (objects.isTruncated());
+                
+                if (bucketItems > 0) {
+                	System.out.println(bucket.getName() + "\t" + bucket.getCreationDate() + "\t" + bucketItems + "\t" + bucketSize + "\t" + lastModified);
+                } else {
+                	System.out.println(bucket.getName() + "\t" + bucket.getCreationDate() + "\t" + bucketItems + "\t" + bucketSize + "\t" + " - -");
+                }
+            }
+
+            System.out.println("\nYou have " + buckets.size() + " Amazon S3 bucket(s), " +
+                    "containing " + totalItems + " objects with a total size of " + totalSize + " bytes.");
+            
+            
+        } catch (AmazonServiceException ase) {
+            /*
+             * AmazonServiceExceptions represent an error response from an AWS
+             * services, i.e. your request made it to AWS, but the AWS service
+             * either found it invalid or encountered an error trying to execute
+             * it.
+             */
+            System.out.println("Error Message:    " + ase.getMessage());
+            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+            System.out.println("Error Type:       " + ase.getErrorType());
+            System.out.println("Request ID:       " + ase.getRequestId());
+        } catch (AmazonClientException ace) {
+            /*
+             * AmazonClientExceptions represent an error that occurred inside
+             * the client on the local host, either while trying to send the
+             * request to AWS or interpret the response. For example, if no
+             * network connection is available, the client won't be able to
+             * connect to AWS to execute a request and will throw an
+             * AmazonClientException.
+             */
+            System.out.println("Error Message: " + ace.getMessage());
+        }
+    }
+}
